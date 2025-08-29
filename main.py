12d5c3d2daf2675cdfe12f87aba848f4feec96e8
daryl0181 <darylangeloseverino@gmail.com>
2025-08-29 23:48:25 +0800

    this is the 4th

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 user_input = input("Enter something: ")
 print(f"You entered: {user_input}")
 # This is a simple Python script that takes user input and prints it back to the console.
-
-
-
-#this is the 3rd commit dsdsd
+ 
+print("Hello, World!")
